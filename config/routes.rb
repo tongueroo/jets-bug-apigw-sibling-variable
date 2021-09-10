0fb85507ccfe4df0f9fbba8409ed6b93d99e63d5
Tung Nguyen <tongueroo@gmail.com>
2021-09-10 00:23:09 +0000

    code to reproduce sibling variable issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Jets.application.routes.draw do
   resources :posts
-  root "jets/public#show"
 
-  # The jets/public#show controller can serve static utf8 content out of the public folder.
-  # Note, as part of the deploy process Jets uploads files in the public folder to s3
-  # and serves them out of s3 directly. S3 is well suited to serve static assets.
-  # More info here: https://rubyonjets.com/docs/extras/assets-serving/
+  # :foo and *catchcall conflicts. Will get this error:
+  #
+  #     A sibling ({catchall+}) of this resource already has a variable path part -- only one is allowed
+  #
+  # get ':foo/posts', to: 'posts#index' - uncomment out this route to reproduce
+
   any "*catchall", to: "jets/public#show"
+
+  root "jets/public#show"
 end
---
+++ db/schema.rb
@@ -0,0 +1,18 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_08_12_162836) do
+
+# Could not dump table "posts" because of following ActiveRecord::StatementInvalid
+#   Mysql2::Error: Unknown table 'check_constraints' in information_schema
+
+end
